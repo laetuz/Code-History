d6b727724fb7ee059c5102f9cb6c45ef9ea02e8e
laetuz <laetuzg@gmail.com>
2023-09-21 11:28:41 +0700

    Initial Commit

---
+++ Code History/Code_HistoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  Code_HistoryApp.swift
+//  Code History
+//
+//  Created by Ryo Martin on 21/09/23.
+//
+
+import SwiftUI
+
+@main
+struct Code_HistoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
